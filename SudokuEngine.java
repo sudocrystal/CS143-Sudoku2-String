17d1c47ec5959a9546414bfb0f1401972ceef4a0
Crystal Hess <chess@shoreline.edu>
2026-05-05 11:40:29 -0700

    Corrects new location for boards

--- SudokuEngine.java
+++ SudokuEngine.java
@@ -1,6 +1,6 @@
 public class SudokuEngine {
 	public static void main(String[] args) {
-		SudokuBoard board = new SudokuBoard("Sudoku2/data1.sdk");
+		SudokuBoard board = new SudokuBoard("boards/data1.sdk");
 		System.out.print(board);
 		System.out.println("Is valid? " + board.isValid());
 		System.out.println("Is solved? " + board.isSolved());
